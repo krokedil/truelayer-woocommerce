7e5f1f19d97d8f5bcf73f25e966a22c180c71b79
MichaelBengtsson <michael@krokedil.se>
2024-03-15 13:23:48 +0100

    Update create payment request to use the SDK

--- classes/requests/post/class-truelayer-request-create-payment.php
+++ classes/requests/post/class-truelayer-request-create-payment.php
@@ -5,10 +5,17 @@
  * @package TrueLayer_For_WooCommerce/Classes/Requests/Post
  */
 
+defined( 'ABSPATH' ) || exit;
+
+use Krokedil_TrueLayer_Dependencies\TrueLayer\Interfaces\Beneficiary\BeneficiaryInterface;
+use Krokedil_TrueLayer_Dependencies\TrueLayer\Interfaces\Payment\PaymentCreatedInterface;
+use Krokedil_TrueLayer_Dependencies\TrueLayer\Interfaces\PaymentMethod\BankTransferPaymentMethodInterface;
+use Krokedil_TrueLayer_Dependencies\TrueLayer\Interfaces\UserInterface;
+
 /**
  * Class TrueLayer_Request_Create_Payment
  */
-class TrueLayer_Request_Create_Payment extends TrueLayer_Request_Post {
+class TrueLayer_Request_Create_Payment extends TrueLayer_Request {
 
 	/**
 	 * WooCommerce Order ID
@@ -24,139 +31,179 @@ class TrueLayer_Request_Create_Payment extends TrueLayer_Request_Post {
 	 */
 	public function __construct( $arguments ) {
 		parent::__construct( $arguments );
-		$this->log_title       = 'Create payment';
-		$this->endpoint        = '/payments';
-		$this->idempotency_key = Truelayer_Helper_Signing::get_uuid();
-		$this->order_id        = $arguments['order_id'];
+		$this->log_title = 'Create payment';
+		$this->order_id  = $arguments['order_id'];
 	}
 
 	/**
-	 * Create the request body.
+	 * Make the request.
 	 *
-	 * @return string
+	 * @return PaymentCreatedInterface|WP_Error
 	 */
-	protected function get_body() {
+	public function request() {
+		$this->client = $this->get_client();
 
-		$order               = wc_get_order( $this->order_id );
-		$settings            = $this->settings;
-		$merchant_account_id = truelayer_get_merchant_account_id( $order->get_currency() );
+		try {
+			return $this->create_payment();
+		}
+		catch (Exception $e) {
+			return new WP_Error( 'tl_create_payment_error', $e->getMessage() );
+		}
+	}
 
-		$body = array(
-			'amount_in_minor' => TrueLayer_Helper_Order::get_order_amount( $order ),
-			'currency'        => get_woocommerce_currency(),
-			'payment_method'  => array(
-				'type'               => 'bank_transfer',
+	/**
+	 * Build the payment object.
+	 *
+	 * @return PaymentCreatedInterface|WP_Error
+	 *
+	 * @throws Exception
+	 */
+	private function create_payment() {
+		$order = wc_get_order( $this->order_id );
 
-				'beneficiary'        => array(
-					'type'                => 'merchant_account',
-					'account_holder_name' => $settings['truelayer_beneficiary_account_holder_name'],
-					'merchant_account_id' => $merchant_account_id,
-				),
-
-				'provider_selection' => array(
-					'type'   => 'user_selected',
-					'filter' => array(
-						'countries' => array( $order->get_billing_country() ),
-					),
-				),
-			),
-            'user'            => array(
-                'name'  => TrueLayer_Helper_Order::get_account_holder_name( $order ),
-                'email' => $order->get_billing_email(),
-            ),
-        );
-
-        if( ! empty( $order->get_billing_address_1() ) ) {
-            $body['user']['address'] = array(
-                'address_line1' => $order->get_billing_address_1(),
-                'address_line2' => $order->get_billing_address_2(),
-                'city' => $order->get_billing_city(),
-                'state' => ! empty( $state = $order->get_billing_state() ) ? $state : 'NA',
-                'zip' => $order->get_billing_postcode(),
-                'country_code' => $order->get_billing_country(),
-            );
-        }
-
-        if( ! empty( $birth_date = TrueLayer_Helper_Order::get_user_date_of_birth( $order ) ) ) {
-            $body['user']['date_of_birth'] = $birth_date;
-        }
-
-		$customer_segment = $this->get_banking_providers();
-        if ( ! empty( $customer_segment ) ) {
-			$body['payment_method']['provider_selection']['filter']['customer_segments'] = $customer_segment;
+		if ( ! $order ) {
+			throw new Exception( 'Invalid order ID' );
 		}
 
-		$release_channel = $this->get_release_channel();
+		$payment = $this->client
+			->payment()
+			->amountInMinor( TrueLayer_Helper_Order::get_order_amount( $order ) )
+			->currency( get_woocommerce_currency() )
+			->paymentMethod( $this->create_bank_transfer_payment_method( $order ) )
+			->user( $this->create_user( $order ) )
+			->create();
+
+		return $payment;
+	}
+
+	/**
+	 * Create the payment method object.
+	 *
+	 * @param WC_Order $order The WooCommerce order object.
+	 * @return BankTransferPaymentMethodInterface
+	 */
+	private function create_bank_transfer_payment_method( $order ) {
+		$payment_method = $this->client
+			->paymentMethod()
+			->bankTransfer()
+			->beneficiary( $this->create_beneficiary( $order ) );
+
+		$payment_method = $this->maybe_add_provider_selection( $payment_method );
+
+		return $payment_method;
+	}
+
+	/**
+	 * Create the beneficiary object.
+	 *
+	 * @param WC_Order $order The WooCommerce order object.
+	 * @return BeneficiaryInterface
+	 */
+	private function create_beneficiary( $order ) {
+		return $this->client->beneficiary()
+			->merchantAccount()
+			->accountHolderName( $this->settings['truelayer_beneficiary_account_holder_name'] ?? '' )
+			->reference( $order->get_order_number() )
+			->merchantAccountId( truelayer_get_merchant_account_id( $order->get_currency() ) );
+	}
+
+	/**
+	 * Maybe add provider selection.
+	 *
+	 * @param BankTransferPaymentMethodInterface $payment_method The payment method object.
+	 *
+	 * @return BankTransferPaymentMethodInterface
+	 */
+	private function maybe_add_provider_selection( $payment_method ) {
+		$release_channel  = $this->get_release_channel();
+		$banking_provider = $this->get_banking_providers();
+
+		if ( empty( $release_channel ) && empty( $banking_provider ) ) {
+			return $payment_method;
+		}
+
+		$provider_filter = $this->client->providerFilter();
+
 		if ( ! empty( $release_channel ) ) {
-			$body['payment_method']['provider_selection']['filter']['release_channel'] = $release_channel;
+			$provider_filter->releaseChannel( $release_channel );
 		}
 
-		return $body;
+		if ( ! empty( $banking_provider ) ) {
+			$provider_filter->customerSegments( $banking_provider );
+		}
+
+		$provider_selection = $this->client->providerSelection()
+			->userSelected()
+			->filter( $provider_filter );
+
+		$payment_method->providerSelection( $provider_selection );
+
+		return $payment_method;
 	}
 
 	/**
-	 * Get the request url.
+	 * Create the user object.
 	 *
-	 * @return string
+	 * @param WC_Order $order The WooCommerce order object.
+	 * @return UserInterface
 	 */
-	protected function get_request_url() {
-		$truelayer_token_url = $this->get_api_url_base() . $this->endpoint;
+	private function create_user( $order ) {
+		$user = $this->client->user()
+			->name( TrueLayer_Helper_Order::get_account_holder_name( $order ) )
+			->email( $order->get_billing_email() );
 
-		return $truelayer_token_url;
+		$date_of_birth = TrueLayer_Helper_Order::get_user_date_of_birth( $order );
+		if ( ! empty( $date_of_birth ) ) {
+			$user->dateOfBirth( $date_of_birth );
+		}
+
+		$user = $this->maybe_add_billing_address( $user, $order );
+
+		return $user;
 	}
 
 	/**
-	 * Request headers.
+	 * Maybe add billing address.
 	 *
-	 * @param array $body The request body.
-	 * @return array
+	 * @param UserInterface $user The user object.
+	 * @param WC_Order $order The WooCommerce order object.
+	 *
+	 * @return UserInterface
 	 */
-	protected function get_request_headers( $body = array() ) {
-		$token = TrueLayer()->api->get_token();
+	private function maybe_add_billing_address( $user, $order ) {
+		if ( empty( $order->get_billing_address_1() ) ) {
+			return $user;
+		}
 
-		return array(
-			'Content-Type'    => 'application/json',
-			'Idempotency-Key' => $this->idempotency_key,
-			'Tl-Signature'    => Truelayer_Helper_Signing::get_tl_signature( $body, $this ),
-			'TL-Agent'        => 'truelayer-woocommerce/' . TRUELAYER_WC_PLUGIN_VERSION,
-			'Authorization'   => "Bearer $token",
-		);
+		$address = $user->address( null );
+
+		if ( $order->get_billing_address_1() ) {
+			$address->addressLine1( $order->get_billing_address_1() );
+		}
+
+		if ( $order->get_billing_address_2() ) {
+			$address->addressLine2( $order->get_billing_address_2() );
+		}
+
+		if ( $order->get_billing_city() ) {
+			$address->city( $order->get_billing_city() );
+		}
+
+		if ( $order->get_billing_postcode() ) {
+			$address->zip( $order->get_billing_postcode() );
+		}
+
+		if ( $order->get_billing_country() ) {
+			$address->countrycode( $order->get_billing_country() );
+		}
+
+		if ( ! empty( $order->get_billing_state() ) ) {
+			$address->state( $order->get_billing_state() );
+		}
+
+		$user->address( $address );
+
+		return $user;
 	}
 
-	/**
-	 * Get request arguments and check request body.
-	 *
-	 * @return bool|array
-	 */
-	protected function get_request_args() {
-		$body = $this->get_body();
-
-		return array(
-			'headers'    => $this->get_request_headers( $body ),
-			'user-agent' => $this->get_user_agent(),
-			'method'     => $this->method,
-			'timeout'    => apply_filters( 'truelayer_request_timeout', 10 ),
-			'body'       => apply_filters( 'truelayer_request_args', wp_json_encode( $body ) ),
-		);
-	}
-
-	/**
-	 * Returns banking providers.
-	 *
-	 * @return array
-	 */
-	private function get_banking_providers() {
-		$banking_providers = empty( $this->settings['truelayer_banking_providers'] ) ? array() : $this->settings['truelayer_banking_providers'];
-		return array_map( 'strtolower', $banking_providers );
-	}
-
-	/**
-	 * Returns the release channel
-	 *
-	 * @return array
-	 */
-	private function get_release_channel() {
-		$release_channel = empty( $this->settings['truelayer_release_channel'] ) ? array() : $this->settings['truelayer_release_channel'];
-		return $release_channel;
-	}
 }
