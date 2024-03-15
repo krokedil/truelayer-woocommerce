a55cfcf4d5ae9012339c46361332e27e54a90f89
MichaelBengtsson <michael@krokedil.se>
2024-03-15 13:24:21 +0100

    Update build_hosted_payment_page_url

--- classes/requests/helpers/class-truelayer-helper-hosted-payment-page-url.php
+++ classes/requests/helpers/class-truelayer-helper-hosted-payment-page-url.php
@@ -19,15 +19,23 @@ class Truelayer_Helper_Hosted_Payment_Page_URL {
 	/**
 	 * Generates the bank choice redirect url.
 	 *
-	 * @param int $order_id The WooCommerce Order ID.
+	 * @param WC_Order|int $order The WooCommerce Order or order id.
 	 * @return string
 	 */
-	public static function build_hosted_payment_page_url( $order_id ) {
+	public static function build_hosted_payment_page_url( $order ) {
+		if ( ! $order instanceof WC_Order ) {
+			$order = wc_get_order( $order );
+		}
+
+		if ( ! $order instanceof WC_Order ) {
+			return '';
+		}
+
 		$settings = get_option( 'woocommerce_truelayer_settings' );
 
 		$url                     = ( 'yes' === $settings['testmode'] ) ? 'https://payment.truelayer-sandbox.com/' : 'https://payment.truelayer.com/';
-		$truelayer_payment_id    = get_post_meta( $order_id, '_truelayer_payment_id', true );
-		$truelayer_payment_token = get_post_meta( $order_id, '_truelayer_payment_token', true );
+		$truelayer_payment_id    = $order->get_meta( '_truelayer_payment_id', true );
+		$truelayer_payment_token = $order->get_meta( '_truelayer_payment_token', true );
 
 		$redirect_uri = rawurlencode( home_url( '/wc-api/TrueLayer_Redirect/' ) );
 
